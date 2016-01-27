4546e54ed29d1f53327629e829c2f24a7b2fd89e
elberdev <elberdev@gmail.com>
2016-01-26 19:55:41 -0500

    fixed typo in article model

--- blog/app/models/article.rb
+++ blog/app/models/article.rb
@@ -2,6 +2,6 @@ class Article < ActiveRecord::Base
 
 	# this line validates that every article has a title of at least 5 
 	# characters
-	validate :title, presence: true, length: { minimum: 5 }
+	validates :title, presence: true, length: { minimum: 5 }
 
 end
