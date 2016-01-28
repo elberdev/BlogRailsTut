6687dda94923cac85dfc6c3ae0ade2272438b5d2
elberdev <elberdev@gmail.com>
2016-01-27 22:35:15 -0500

    fixed article model to include comments

--- blog/app/models/article.rb
+++ blog/app/models/article.rb
@@ -1,7 +1,8 @@
 class Article < ActiveRecord::Base
 
-	# this line validates that every article has a title of at least 5 
-	# characters
-	validates :title, presence: true, length: { minimum: 5 }
+  has_many :comments
+  # this line validates that every article has a title of at least 5
+  # characters
+  validates :title, presence: true, length: { minimum: 5 }
 
 end
