1ab6d6e239e13fc6634642b1f9c700c02211a295
elberdev <elberdev@gmail.com>
2016-01-29 22:29:17 -0500

    added authentication requirements to articles_controller

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,4 +1,8 @@
 class ArticlesController < ApplicationController
+
+  # this line requires authentication for every action except index and show
+  http_basic_authenticate_with name: "elbotron" password:"secret_sauce", except: [:index, :show]
+
   def index
     @articles = Article.all
   end
