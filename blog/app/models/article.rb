e19b04cea0e6bd3baa5b950c22ce9481fcfe3b83
elberdev <elberdev@gmail.com>
2016-01-29 22:25:55 -0500

    added dependent option in Article model to destroy associated comments when an article is destroyed

--- blog/app/models/article.rb
+++ blog/app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ActiveRecord::Base
 
-  has_many :comments
+  has_many :comments, dependent: :destroy
   # this line validates that every article has a title of at least 5
   # characters
   validates :title, presence: true, length: { minimum: 5 }
